dd03c2d750da84fd31b73198a2f49268b0702a8a
ronovapromotelibrary-cmd <ronovapromotelibrary@gmail.com>
2026-08-20 11:33:11 +0000

    Fix Kotlin and Compose JVM compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,10 @@ android {
     debug { signingConfig = signingConfigs.getByName("debugConfig") }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
+  kotlin { jvmToolchain(17) }
   buildFeatures {
     compose = true
     buildConfig = true
